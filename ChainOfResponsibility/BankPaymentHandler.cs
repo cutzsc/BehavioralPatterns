ddbd2df5a7737eb187382335f532839e8ef2f7e9
Ildar <tag.ildar@gmail.com>
2019-08-09 09:47:02 +0300

    Реализация паттерна Chain of responsibility.

---
+++ ChainOfResponsibility/BankPaymentHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+	class BankPaymentHandler : PaymentHandler
+	{
+		public override void Handle(Receiver receiver)
+		{
+			if (receiver.BankTransfer)
+				Console.WriteLine("Выполняем банковский перевод");
+			else
+				Successor?.Handle(receiver);
+		}
+	}
+}
--- ChainOfResponsibility/Program.cs
+++ ChainOfResponsibility/Program.cs
@@ -10,6 +10,18 @@ namespace ChainOfResponsibility
 	{
 		static void Main(string[] args)
 		{
+			Receiver receiver = new Receiver(false, true, true);
+
+			PaymentHandler bank = new BankPaymentHandler();
+			PaymentHandler money = new MoneyPaymentHandler();
+			PaymentHandler pp = new PayPalPaymentHandler();
+
+			bank.Successor = pp;
+			pp.Successor = money;
+
+			bank.Handle(receiver);
+
+			Console.ReadLine();
 		}
 	}
 }
