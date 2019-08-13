3e6e36f1be6e8e7680ecd247cb8cd8c37d078ea4
Ildar <tag.ildar@gmail.com>
2019-08-13 19:15:32 +0300

    Реализация паттерна Mediator

--- Mediator/Program.cs
+++ Mediator/Program.cs
@@ -10,6 +10,19 @@ namespace Mediator
 	{
 		static void Main(string[] args)
 		{
+			ManagerMediator m = new ManagerMediator();
+			CustomerColleague customer = new CustomerColleague(m);
+			ProgrammerColleague programmer = new ProgrammerColleague(m);
+			TesterColleague tester = new TesterColleague(m);
+			m.Customer = customer;
+			m.Programmer = programmer;
+			m.Tester = tester;
+
+			customer.Send("Есть заказ, надо сделать программу");
+			programmer.Send("Программа готова, надо протестировать");
+			tester.Send("Программа протестирована и готова к продаже");
+
+			Console.ReadLine();
 		}
 	}
 }
---
+++ Mediator/ProgrammerColleague.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator
+{
+	class ProgrammerColleague : Colleague
+	{
+		public ProgrammerColleague(Mediator m)
+			: base(m) { }
+
+		public override void Notify(string message)
+		{
+			Console.WriteLine("Сообщение программисту: " + message);
+		}
+	}
+}
